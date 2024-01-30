0766e0250b52491d7b819a34b3e09fdc78e06921
jp-suzuki <suzukij.2000@gmail.com>
2024-01-30 23:00:19 +0900

    JS実践課題１初回提出

---
+++ main.js
@@ -0,0 +1,63 @@
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let milliSeconds = 0;
+let timerRunning = null;
+
+function zeroPadding(inputValue){
+    let outputValue = null;
+    if(inputValue / 10 < 1){
+        outputValue = "0" + inputValue;
+    }else{
+        outputValue = inputValue;
+    }
+    return outputValue;
+}
+function displayTimes(){
+    $(".hoursDisplay").text(zeroPadding(hours) + ":");
+    $(".minutesDisplay").text(zeroPadding(minutes) + ":");
+    $(".secondsDisplay").text(zeroPadding(seconds) + ":");
+    $(".milliSecondsDisplay").text(zeroPadding(milliSeconds));
+}
+function timerCountUp(){
+    milliSeconds += 1;
+    if(milliSeconds == 100){
+        milliSeconds = 0;
+        seconds += 1;
+    }
+    if(seconds == 60){
+        seconds = 0;
+        minutes += 1;
+    }
+    if(minutes == 60){
+        minutes = 0;
+        hours += 1;
+    }
+    displayTimes();
+}
+
+$(document).ready(function(){
+    $(".buttonStart").click(function() {
+        timerRunning = setInterval(timerCountUp, 10);
+        $("button.buttonStart").prop("disabled", true);
+        $("button.buttonStop").prop("disabled", false);
+        $("button.buttonReset").prop("disabled", false);
+    });
+    $(".buttonStop").click(function(){
+        clearInterval(timerRunning);
+        $("button.buttonStart").prop("disabled", false);
+        $("button.buttonStop").prop("disabled", true);
+        $("button.buttonReset").prop("disabled", false);
+    });
+    $(".buttonReset").click(function(){
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+        milliSeconds = 0;
+        clearInterval(timerRunning);
+        displayTimes();
+        $("button.buttonStart").prop("disabled", false);
+        $("button.buttonStop").prop("disabled", true);
+        $("button.buttonReset").prop("disabled", true);
+    });
+});
